5d46501b768ac01e44614f38c16741fedb27dd91
inmajhossain <inmaj670@gmail.com>
2025-04-14 03:04:17 +0600

    Add: Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,78 +1,24 @@
-"use client";
-import Link from "next/link";
 import React from "react";
-import { Button } from "../ui/button";
-import { MoonIcon, SunIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
-import { useTheme } from "next-themes";
-
-const navigation = [
-  {
-    name: "Home",
-    href: "/",
-  },
-  {
-    name: "About",
-    href: "/about",
-  },
-  {
-    name: "Portfolio",
-    href: "/portfolio",
-  },
-  {
-    name: "Contact",
-    href: "/contact",
-  },
-  {
-    name: "Articles",
-    href: "/articles",
-  },
-];
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/image/logo.png";
+import { ArrowRight } from "lucide-react";
 
 export default function Navbar() {
-  const pathname = usePathname();
-
-  const { theme, setTheme } = useTheme();
-
-  console.log(theme);
-
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <nav className="max-w-custom mx-auto flex h-16 items-center">
-        <div className="mr-8">
-          <Link href={"/"} className="text-2xl font-bold">
-            Hustle Articles
-          </Link>
-        </div>
-        <div className="flex flex-1 items-center justify-between">
-          <div className="flex items-center space-x-6">
-            {navigation.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className={cn(
-                  "text-sm font-medium transition-colors hover:text-primary",
-                  pathname === item.href
-                    ? "text-foreground"
-                    : "text-foreground/60"
-                )}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <Button
-          variant="ghost"
-          size="icon"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+    <div className="flex flex-row justify-between items-center bg-gray-800 lg:ml-[16px] p-[17px] rounded-full lg:h-[84px] container">
+      <Link href={"/"}>
+        <Image src={logo} alt={"logo"} width={37} height={37} />
+      </Link>
+      <nav className="hidden lg:flex flex-row items-center gap-[32px]">
+        <Link href={"/all-pages"}>All Pages</Link>
+        <Link href={"/ntegrations"}>Integrations</Link>
+        <Link href={"/pricing"}>Pricing</Link>
+        <Link href={"/contact"}>Contact</Link>
+        <button className="flex justify-around items-center bg-accent mx-auto lg:ml-[66px] rounded-full w-[207px] h-[48px]">
+          Get Started Now <ArrowRight />
+        </button>
       </nav>
-    </header>
+    </div>
   );
 }
