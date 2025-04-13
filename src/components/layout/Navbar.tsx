2e5b98e747f7cf074b7ffdbb84b3b3539dd36cbf
inmajhossain <inmaj670@gmail.com>
2025-04-14 03:25:40 +0600

    Update: Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-row justify-between items-center bg-gray-800 p-[17px] rounded-full lg:h-[84px] container">
+    <div className="flex flex-row justify-between items-center bg-gray-800 mt-[40px] p-[17px] rounded-full lg:h-[84px] container">
       <Link href={"/"}>
         <Image
           src={logo}
@@ -15,12 +15,14 @@ export default function Navbar() {
           className="lg:ml-[16px]"
         />
       </Link>
-      <nav className="hidden lg:flex flex-row items-center gap-[32px]">
-        <Link href={"/all-pages"}>All Pages</Link>
-        <Link href={"/ntegrations"}>Integrations</Link>
-        <Link href={"/pricing"}>Pricing</Link>
-        <Link href={"/contact"}>Contact</Link>
-        <button className="flex justify-around items-center bg-accent mx-auto lg:ml-[66px] rounded-full w-[207px] h-[48px]">
+      <nav className="hidden lg:flex flex-row items-center gap-[98px] mr-0">
+        <div className="flex flex-row- gap-[32] w-[395px]">
+          <Link href={"/all-pages"}>All Pages</Link>
+          <Link href={"/ntegrations"}>Integrations</Link>
+          <Link href={"/pricing"}>Pricing</Link>
+          <Link href={"/contact"}>Contact</Link>
+        </div>
+        <button className="flex justify-around items-center bg-accent mx-auto rounded-full w-[207px] h-[48px]">
           Get Started Now <ArrowRight />
         </button>
       </nav>
