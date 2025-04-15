16cac43ab0259a8dc3d04b5f6463e86f132b5a66
mahbub-alom <mdmahbuba034@gmail.com>
2025-04-15 22:41:09 +0600

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import Hero from "./home/hero/Hero";
+import Hero from "@/components/home/hero/Hero";
+
 
 export default function Home() {
   return (
