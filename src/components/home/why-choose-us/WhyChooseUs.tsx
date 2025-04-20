00846a9e5f2d857f1bfdb54f27dae3c2731002aa
inmajhossain <inmaj670@gmail.com>
2025-04-20 16:48:24 +0600

    Update: Components Details

--- src/components/home/why-choose-us/WhyChooseUs.tsx
+++ src/components/home/why-choose-us/WhyChooseUs.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const WhyChooseUs = () => {
-  return <div>WhyChooseUs</div>;
+  return (
+    <div className="flex flex-col justify-between items-center w-full lg:w-[1224px] lg:h-[1207px]">
+      {/* First Start */}
+      <div></div>
+      {/* First End */}
+      {/* Second Start */}
+      {/* Second End */}
+      {/* Third Start */}
+      {/* Third End */}
+      {/* Fourth Start */}
+      {/* Fourth End */}
+    </div>
+  );
 };
 
 export default WhyChooseUs;
