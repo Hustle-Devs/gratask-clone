ee6121216e435dd897a76ff9fb74d5458ccf17b0
aimanshafi <shafiaiman88@gmail.com>
2025-04-21 23:24:43 +0600

    Update: Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import ClientLogo from "@/components/home/client-logo/ClientLogo";
 import Hero from "@/components/home/hero/Hero";
 import PricingPlan from "@/components/home/pricing-table/PricingTable";
 import WhyChooseUs from "@/components/home/why-choose-us/WhyChooseUs";
+import Testimonials from "@/components/testimonials/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <ClientLogo />
       <WhyChooseUs />
       <PricingPlan />
+      <Testimonials />
     </div>
   );
 }
